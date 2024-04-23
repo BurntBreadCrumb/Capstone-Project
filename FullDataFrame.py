306f672aa96eeda3fb36e749ed2a6e280359ebd4
BurntBreadCrumb <90420559+BurntBreadCrumb@users.noreply.github.com>
2024-04-22 23:01:11 -0500

    Update FullDataFrame.py

--- FullDataFrame.py
+++ FullDataFrame.py
@@ -1,9 +1,3 @@
-#things I want my new fun dataframe compiler to do
-#input=directories which contain all of my data :o
-    #first: give one directory (7_23_22) and ask it to run birdnet on each file within East, South, and West
-    #as it goes i want to save birdnet in a list, and also date and time to differentiate files
-#output = dataframe containing all output (birdNET dataframe but also include the date (from file name) and time (also from file name))
-    #will have to store all of the information in a list (with birdnetlib already days hooray) then convert the list into pandas df
 # import required module
 import os 
 from pathlib import Path
@@ -13,9 +7,6 @@ import pandas as pd
 from birdnetlib import Recording
 from birdnetlib.analyzer import Analyzer
 from datetime import datetime
-
-#def function = inputs: year and recorder direction and hourly? 
-#should include a branch for one file or multiple files #WIP
 
 directory = "TestDirectory/"
 #East, West, South
@@ -69,11 +60,3 @@ print(df) #CHECKPOINT
 #EDIT FILENAME
 name = "JuneDetections"
 df.to_csv(name+'.txt',  sep='\t', index=False)
-
-
-###https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
-'''        data_folder = Path(directory)
-
-        file_to_open = data_folder / file
-        f = file_to_open 
-'''
